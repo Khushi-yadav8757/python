661bd9e4754c283b3af9cf985c219d25895a763e
Khushi Yadav <kky957242@gmail.com>
2025-03-18 23:13:58 +0530

    Create age.py

---
+++ age.py
@@ -0,0 +1,8 @@
+# Eligibe for vote or not 
+
+
+Age = int(input("Enter the Age : "))
+if Age>=18:
+    print("You are Eligible for the vote ")
+else:
+    print("Sorry,you are Not Eligible for the vote ")    
