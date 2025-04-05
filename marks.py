fcbcb6c398bff40124c3477c566c75f048ba09c4
Khushi Yadav <kky957242@gmail.com>
2025-04-05 16:40:40 +0530

    Create marks.py

---
+++ marks.py
@@ -0,0 +1,9 @@
+Marks = int(input("Enter the Marks : "))
+if Marks > 80 :
+    print("Very Good ")
+elif Marks > 60 :
+    print("Good ")
+elif Marks > 40 :
+    print("Average ")
+else:
+    print("Fail ")            
