4203655d8f8952379598c610bb4e609570a39fba
Khushi Yadav <kky957242@gmail.com>
2025-04-05 19:27:44 +0530

    Create greater.py

---
+++ greater.py
@@ -0,0 +1,9 @@
+x=int(input("Enter the 1st positive number:"))
+y=int(input("Enter the 2nd positive number:"))
+z=int(input("Enter the 3rd positive number:"))
+if x>y and x>z:
+    print("1st number is greater ")
+elif y>x and y>z:
+    print("2nd number is greater ")
+else:
+    print("3rd number is greater ")        
