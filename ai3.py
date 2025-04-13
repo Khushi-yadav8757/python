f5c6fdd2ba96946d02177fd560bee06ef7665bdb
Khushi Yadav <kky957242@gmail.com>
2025-04-14 00:12:50 +0530

    Create ai3.py

---
+++ ai3.py
@@ -0,0 +1,16 @@
+a = int(input("Enter the 1st number:"))
+b = int(input("Enter the 2nd number:"))
+c = int(input("Enter the 3rd number:"))
+
+print("sum of these numbers=",a+b+c)
+print("multiply of these numbers=",a*b*c)
+print("difference of these numbers=",a-b-c)
+
+bday_yr = int(input("Enter your birthday year:"))
+print("your age in 2025:", 2025 - bday_yr)
+
+x=int(input("Enter a number:"))
+if x%2==0:
+    print("even")
+else:
+    print("odd")
