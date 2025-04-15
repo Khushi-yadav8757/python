b216fe9869966cc3d33ec3a6cd3a80727677c861
Khushi Yadav <kky957242@gmail.com>
2025-04-15 22:01:44 +0530

    Create login.py

---
+++ login.py
@@ -0,0 +1,7 @@
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "khushi" and password == "1234":
+    print("Login Successful!")
+else:
+    print("Login Failed!")
