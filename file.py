6f4c51607d97a6d9ca9144a33e83502107cef7f9
Khushi Yadav <kky957242@gmail.com>
2025-05-06 00:57:07 +0530

    Create file.py

---
+++ file.py
@@ -0,0 +1,16 @@
+file = open("data.txt", "r")  # modes: 'r', 'w', 'a', 'r+', 'w+', 'a+'
+
+📌 Common Modes:
+
+'r' → Read (default), file must exist
+
+'w' → Write, overwrites file
+
+'a' → Append to end
+
+'r+' → Read + Write, doesn't truncate
+
+'w+' → Write + Read, truncates file
+
+'a+' → Append + Read 
+
