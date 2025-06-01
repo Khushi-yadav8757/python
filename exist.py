c5c39e386bfd3be9b8ca0366c12e5cb80f38a652
Khushi Yadav <kky957242@gmail.com>
2025-06-02 00:20:25 +0530

    Create exist.py

---
+++ exist.py
@@ -0,0 +1,7 @@
+#program to check if an element exists in a list
+nums = [1, 2, 3, 4, 5]
+x = int(input("Enter number to check: "))
+if x in nums:
+    print("Exists")
+else:
+    print("Does not exist")
