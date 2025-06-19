0a8d1b2f27356646fb8453fd60aec1fcc086c4f1
Khushi Yadav <kky957242@gmail.com>
2025-06-19 17:27:31 +0530

    Create makelist.py

---
+++ makelist.py
@@ -0,0 +1,4 @@
+#Make a list of even numbers from 2 to 20 using list comprehension
+even_numbers = [x for x in range(2, 21, 2)]
+
+print(even_numbers)  # Output: [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
