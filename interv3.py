ae2747bcc8be75740c601ec86687e9fc3a83142f
Khushi Yadav <kky957242@gmail.com>
2025-06-20 20:19:55 +0530

    Create interv3.py

---
+++ interv3.py
@@ -0,0 +1,7 @@
+-What is a higher-order function?
+A function that takes another function as an argument or returns a function.
+ex-
+def add(x):
+    return lambda y: x + y
+f = add(10)
+print(f(5))  # Output: 15
