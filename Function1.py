67e5297448eb569bef0b187921d45605d7487dd1
Khushi Yadav <kky957242@gmail.com>
2025-07-16 23:29:23 +0530

    Create Function1.py

---
+++ Function1.py
@@ -0,0 +1,8 @@
+Functionis the block of code which only runs when is called 
+function is also used for reusablility of code 
+python function starts with "def" keyword
+
+now,
+there are three (03) types of python function
+1. build in - print(), range(), len(), round(), max(),min().
+2. user defined- 
