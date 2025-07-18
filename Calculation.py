3c5c024b649541ae97653a813e6546f6c4eb8d05
Khushi Yadav <kky957242@gmail.com>
2025-07-18 23:52:18 +0530

    Create Calculation.py

---
+++ Calculation.py
@@ -0,0 +1,11 @@
+# Write a function calculation() such that it accepts two variables and calculation the addition & subtraction & division.
+
+def calculation(a,b):
+    print("A :",a)
+    print("B :",b)
+    print("Sum of A and B :",a+b)
+    print("Sub of A and B :",a-b)
+    print("Div of A and B :",a//b)
+
+    #calling the function
+calculation(10,5)   
