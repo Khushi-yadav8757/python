3ad42491b8fd6cdc3efd0abd36b9a0b6cc2df678
Khushi Yadav <kky957242@gmail.com>
2025-07-19 01:27:10 +0530

    Create randint_function.py
    Write a Python program to generate and print a random integer between 1000 and 5000.

---
+++ randint_function.py
@@ -0,0 +1,7 @@
+#Write a Python program to generate and print a random integer between 1000 and 5000.
+
+from random import randint
+number = randint(1000, 5000)                                 #likewise we generate otp (when u run it gives random values )
+print("Your random number is:", number)
+
+
