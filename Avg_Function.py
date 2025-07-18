a197542c014421d1a0da571ec5b831367dac4734
Khushi Yadav <kky957242@gmail.com>
2025-07-18 22:39:31 +0530

    Create Avg_Function.py

---
+++ Avg_Function.py
@@ -0,0 +1,14 @@
+#a=23
+#b=37
+#c=75
+#avg=(a+b+c)/3
+#print(avg)
+
+def avg():
+    a=int(input("enter your number: "))
+    b=int(input("enter your number: "))
+    c=int(input("enter your number: "))
+
+    average=(a+b+c)/3
+    print(average)
+avg()
