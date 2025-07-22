41978f6513384e67a5aad0bd9629a252c9e6ee88
Khushi Yadav <kky957242@gmail.com>
2025-07-22 23:05:21 +0530

    Create SquarePattern.py

---
+++ SquarePattern.py
@@ -0,0 +1,14 @@
+# Print a Square of Stars
+n = int(input("Enter size of square: "))
+
+for i in range(n):
+    for j in range(n):
+        print("*", end=" ")
+    print()
+
+
+#output is :  
+* * * *
+* * * *
+* * * *
+* * * *
