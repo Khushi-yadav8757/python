d5926661f90fe66239ab205699427f072718b6a1
Khushi Yadav <kky957242@gmail.com>
2025-08-07 18:16:18 +0530

    Create File_Handling.py

---
+++ File_Handling.py
@@ -0,0 +1,11 @@
+#File Open Karna – open() function
+file = open("example.txt", "w")  # w = write mode
+
+#Writing to a File – write()
+Writing to a File – write()
+
+file = open("example.txt", "r")
+content = file.read()
+print(content)
+file.close()
+
