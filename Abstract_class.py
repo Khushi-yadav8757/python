1008ea6f09091a86f95409bfecc60442c0e0a71a
Khushi Yadav <kky957242@gmail.com>
2025-09-07 20:57:49 +0530

    Create Abstract_class.py

---
+++ Abstract_class.py
@@ -0,0 +1,18 @@
+Q6.Use abc module to create abstract class Shape with method area().
+Ans:-
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+c = Circle(5)
+print(c.area())   # 78.5
