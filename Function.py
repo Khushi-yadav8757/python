c47bbc6bbaec4fd1d0240db66715ba7884330912
Khushi Yadav <kky957242@gmail.com>
2025-09-17 18:17:20 +0530

    Create Function.py

---
+++ Function.py
@@ -0,0 +1,7 @@
+A function is a block of code which only runs when it is called.
+You can pass data, known as parameters, into a function.
+A function can return data as a result.
+
+#Define function in python 
+  def my_function():
+  print("Hello from a function")
