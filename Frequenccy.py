1398dd8eb41f7f9b04359a4d1d29e4f7b18eff76
Khushi Yadav <kky957242@gmail.com>
2025-09-27 19:14:05 +0530

    Create Frequenccy.py

---
+++ Frequenccy.py
@@ -0,0 +1,10 @@
+#Count frequency of elements in a list
+Ans:
+def count_frequency(lst):
+    freq = {}
+    for item in lst:
+        freq[item] = freq.get(item, 0) + 1
+    return freq
+
+print(count_frequency([1, 2, 2, 3, 1, 4, 2]))
+# Output: {1: 2, 2: 3, 3: 1, 4: 1}
