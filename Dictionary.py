962daa0081a81ea004e7b988062c3905f7ef2fb4
Khushi Yadav <kky957242@gmail.com>
2025-09-27 19:21:31 +0530

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,8 @@
+#Sort a dictionary by values
+Ans:
+def sort_dict_by_value(d):
+    return dict(sorted(d.items(), key=lambda x: x[1]))
+
+my_dict = {"a": 3, "b": 1, "c": 2}
+print(sort_dict_by_value(my_dict))
+# Output: {'b': 1, 'c': 2, 'a': 3}
