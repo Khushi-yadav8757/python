b2602454c5a2a64721512ea9d62f6486e4a0827d
Khushi Yadav <kky957242@gmail.com>
2025-10-07 21:32:54 +0530

    Update property.py

--- property.py
+++ property.py
@@ -1,7 +1,7 @@
 #Explain property() and @property.
 Ans:
-Used to define getters/setters in a Pythonic way.
 
+Used to define getters/setters in a Pythonic way.
 class Person:
     def __init__(self, name):
         self._name = name
