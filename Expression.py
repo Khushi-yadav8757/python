8d85a15ca479a1206913a5326613246a690f49df
Khushi Yadav <kky957242@gmail.com>
2025-10-07 21:31:28 +0530

    Update Expression.py

--- Expression.py
+++ Expression.py
@@ -1,7 +1,7 @@
 # What is list comprehension vs generator expression?
 Ans:-
-List comprehension → creates list in memory.
 
+List comprehension → creates list in memory.
 Generator expression → creates iterator (lazy).
 
 lst = [x*x for x in range(5)]   # list
