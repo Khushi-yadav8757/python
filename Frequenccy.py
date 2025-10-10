ea996da81b227073f6f582e52b9b5e0027c4cad1
Khushi Yadav <kky957242@gmail.com>
2025-10-10 23:01:45 +0530

    Update Frequenccy.py

--- Frequenccy.py
+++ Frequenccy.py
@@ -5,6 +5,6 @@ def count_frequency(lst):
     for item in lst:
         freq[item] = freq.get(item, 0) + 1
     return freq
-
+    
 print(count_frequency([1, 2, 2, 3, 1, 4, 2]))
 # Output: {1: 2, 2: 3, 3: 1, 4: 1}
