dc5b82afbd5a05145e439a707518b6254c4eda06
Khushi Yadav <kky957242@gmail.com>
2025-10-10 22:55:56 +0530

    Update Function.py

--- Function.py
+++ Function.py
@@ -1,3 +1,4 @@
+#what is function
 A function is a block of code which only runs when it is called.
 You can pass data, known as parameters, into a function.
 A function can return data as a result.
