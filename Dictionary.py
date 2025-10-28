20f32b7b9a2a7d7381070ed7352d36b5c91cf586
Khushi Yadav <kky957242@gmail.com>
2025-10-28 20:51:37 +0530

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -2,7 +2,7 @@
 
 def sort_dict_by_value(d):
     return dict(sorted(d.items(), key=lambda x: x[1]))
-
+    
 my_dict = {"a": 3, "b": 1, "c": 2}
 print(sort_dict_by_value(my_dict))
 # Output: {'b': 1, 'c': 2, 'a': 3}
